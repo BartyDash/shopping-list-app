ec2619f8f3fefe70751855c64ccc02e70d691cc8
BartyDash <writetobartek@gmail.com>
2024-01-23 00:32:22 +0100

    firebase config & item adding feature

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, push, ref } from 'firebase/database';
+
+const firebaseSettings = {
+    databaseURL: 'https://shopping-list-app-9a087-default-rtdb.europe-west1.firebasedatabase.app/'
+};
+
+const app = initializeApp(firebaseSettings);
+const database = getDatabase(app);
+const shoppingListDatabase = ref(database, 'shoppingList');
+
 const inputField = document.querySelector('.input-field--js');
 const addButton = document.querySelector('.add-button--js');
 
 addButton.addEventListener('click', () => {
     let inputValue = inputField.value;
-    alert('click!' + inputValue);
+
+    push(shoppingListDatabase, inputValue);
+    inputField.value = '';
+
+    console.log(inputValue);
 });
