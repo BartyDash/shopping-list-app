f06fddcc2739181395fdc9272d50d6f8efb3fc60
BartyDash <writetobartek@gmail.com>
2024-01-23 22:21:35 +0100

    get items from database & show

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, push, ref } from 'firebase/database';
+import { getDatabase, onValue, push, ref } from 'firebase/database';
 
 const firebaseSettings = {
     databaseURL: 'https://shopping-list-app-9a087-default-rtdb.europe-west1.firebasedatabase.app/'
@@ -11,12 +11,35 @@ const shoppingListDatabase = ref(database, 'shoppingList');
 
 const inputField = document.querySelector('.input-field--js');
 const addButton = document.querySelector('.add-button--js');
+const shoppingList = document.querySelector('.shopping-list--js');
 
 addButton.addEventListener('click', () => {
     let inputValue = inputField.value;
 
     push(shoppingListDatabase, inputValue);
-    inputField.value = '';
 
-    console.log(inputValue);
+    clearInputField();
 });
+
+onValue(shoppingListDatabase, (snapshot) => {
+    let itemsArray = Object.values(snapshot.val());
+
+    clearShoppingList();
+    
+    for (let element of itemsArray) {
+        appendItemShoppingList(element);
+    }
+});
+
+function clearInputField() {
+    inputField.value = '';
+}
+
+function clearShoppingList() {
+    shoppingList.innerHTML = '';
+}
+
+function appendItemShoppingList(inputValue) {
+    let item = `<li>${inputValue}</li>`;
+    shoppingList.insertAdjacentHTML('beforeend', item);
+}
